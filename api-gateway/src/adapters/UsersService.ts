abcacac905f831c6cafefecda65ea2b0ddfc5cca
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2021-05-20 16:42:23 -0400

    refactor

--- api-gateway/src/adapters/UsersService.ts
+++ api-gateway/src/adapters/UsersService.ts
@@ -7,24 +7,26 @@ export interface User {
   id: string;
   username: string;
 }
+
 export interface UserSession {
   createdAt: string;
   expiresAt: string;
   id: string;
   userId: string;
 }
+
 export default class UsersService {
-  static async createUser({ password, username}: {password: string, username: string}) {
-    const body = await got.post(`${USERS_SERVICE_URI}/users`, { json: {password, username}}).json();
+  static async createUser({ password, username }: { password: string; username: string }) {
+    const body = await got.post(`${USERS_SERVICE_URI}/users`, { json: { password, username } }).json();
     return body;
   }
 
-  static async createUserSession ({ password, username}: { password: string; username: string}) {
-    const body = <UserSession>await got.post(`${USERS_SERVICE_URI}/sessions`, { json: { password, username} }).json();
+  static async createUserSession({ password, username }: { password: string; username: string }) {
+    const body = <UserSession>await got.post(`${USERS_SERVICE_URI}/sessions`, { json: { password, username } }).json();
     return body;
   }
 
- static async deleteUserSession({ sessionId }: { sessionId: string }) {
+  static async deleteUserSession({ sessionId }: { sessionId: string }) {
     const body = await got.delete(`${USERS_SERVICE_URI}/sessions/${sessionId}`).json();
     return body;
   }
